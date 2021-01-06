97340dd3f7cca5f73a8b4e79898044de32164d20
Lisander Lopez <lisander@buffalo.edu>
2021-01-06 16:10:38 -0500

    Bug: Theme is not initialized on first render

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
-import '../styles/globals.scss'
-import { ThemeProvider } from 'next-themes'
+import '../styles/globals.scss';
+import { ThemeProvider, useTheme } from 'next-themes';
 import {GlobalProvider} from '../context/globalContext';
 import { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
   
   const [mounted, setMounted] = useState(false);
-  
+  //const { theme, setTheme } = useTheme();
   // When mounted on client, now we can show the UI, Taken from DOCS
   // I think we are making sure server theme == client theme 
   useEffect(() => setMounted(true), [])
 
   if (!mounted) return null;
+
+  // if (localStorage.getItem("theme") === null) {
+    
+  // }
 
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
